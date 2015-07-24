95c2c62b403195cd36aad9a7dc787c10a90f489c
egorrishe <egorrishe@yandex.ru>
2015-07-24 23:51:37 +0300

    Fixes: 	- Dropzone options had no effect 	- if 'customStyle' is empty, then yii register file '0'
    Other:
    	- updated dropzone to 4.0.1
    	- refactoring: now it is easier to override widget
    	- added translations
    	- added fallback HTML
    	- now jQuery is not required

---
+++ messages/es/dropzone.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'<b>Drop files</b> here to upload <span>(or click)</span>' => '<b>Пертяните файлы</b> для загрузки <span>(или клик)</span>',
+	"Your browser does not support drag'n'drop file uploads." => "Ваш браузер не поддерживает способ загрузки файлов drag'n'drop.",
+	'Please use the fallback form below to upload your files like in the olden days.' => 'Пожалуйста, воспользуйтесь формой ниже, для загрузки файлов по старинке.',
+	'Wrong type. Allowed types are: {types}' => 'El archivo incorrecto. Tipos permitidos son: {types}',
+	'Size is too big. Allowed size is {{maxFilesize}} MB. Your file is {{filesize}} MB' => 'El archivo es demasiado grande ({{filesize}}). El máximo tamaño permitido es {{maxFilesize}}.',
+);
---
+++ messages/ru/dropzone.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'<b>Drop files</b> here to upload <span>(or click)</span>' => '<b>Перетяните файлы</b> для загрузки <span>(или клик)</span>',
+	"Your browser does not support drag'n'drop file uploads." => "Ваш браузер не поддерживает способ загрузки файлов drag'n'drop.",
+	'Please use the fallback form below to upload your files like in the olden days.' => 'Пожалуйста, воспользуйтесь формой ниже, для загрузки файлов по старинке.',
+	'Wrong type. Allowed types are: {types}' => 'Неправильный файл. Допустимые типы: {types}',
+	'Size is too big. Allowed size is {{maxFilesize}} MB. Your file is {{filesize}} MB' => 'Файл слишком большой. Максимально допустимый размер: {{maxFilesize}} MB. Размер Вашего файла: {{filesize}} MB',
+);
